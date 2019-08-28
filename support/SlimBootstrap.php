31ac4cc13fcff9bf845354fbbd36ecf0336a6dec
Andrzej Heczko <heczko@logio.cz>
2019-08-28 13:31:50 +0200

    update SlimBootstrap

--- support/SlimBootstrap.php
+++ support/SlimBootstrap.php
@@ -3,13 +3,33 @@ namespace BigBIT\Oddin\Support;
 
 use BigBIT\Oddin\Bootstrap;
 use Slim\DefaultServicesProvider;
+use Slim\Collection;
 
 /**
  * Class SlimBootstrap
  * @package BigBIT\Oddin\Support
  */
 class SlimBootstrap extends Bootstrap {
+    public static $defaultSettings = [
+        'httpVersion' => '1.1',
+        'responseChunkSize' => 4096,
+        'outputBuffering' => 'append',
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => false,
+        'addContentLengthHeader' => true,
+        'routerCacheFile' => false,
+    ];
+
+    /**
+     * @param array $bindings
+     */
     final protected static function boot(array $bindings) {
+        $userSettings = &$bindings['settings'];
+
+        $bindings['settings'] = function() use ($userSettings) {
+            return new Collection(array_merge(static::$defaultSettings, $userSettings));
+        };
+
         parent::boot($bindings);
         $serviceProvider = new DefaultServicesProvider();
 
